40ba8cdb997643a22ff6be52ffebd8334cc6186c
Ward Forthmann <wardforthmann@gmail.com>
2021-05-08 10:46:54 -0700

    Added intial support for viewing files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{fs::File, io::Write};
+use std::{fs::{self, File}, io::Write, path::Path};
 
 use actix_web::{
     middleware,
     web::{self, Query},
-    App, Error, HttpResponse, HttpServer,
+    App, Error, HttpResponse,HttpRequest, HttpServer,
 };
 use chrono::prelude::*;
 use futures::StreamExt;
@@ -68,16 +68,29 @@ async fn save_file(
     Ok(HttpResponse::Ok().body(response))
 }
 
-fn index() -> HttpResponse {
-    let html = r#"<html>
-        <head><title>Upload Test</title></head>
-        <body>
-            <form target="/" method="post" enctype="multipart/form-data">
-                <input type="file" multiple name="file"/>
-                <button type="submit">Submit</button>
-            </form>
-        </body>
-    </html>"#;
+fn index(req: HttpRequest) -> HttpResponse {
+    let called_path : String = req.match_info().get("path").unwrap().parse::<String>().unwrap();
+    let mut from = String::from("./");
+    from.push_str(&called_path);
+    let mut html = String::new();
+    if Path::new(&from).is_dir() {
+        let paths = fs::read_dir(from).unwrap();
+        for path in paths {
+            let path_buf = path.unwrap().path();
+            let path_display = path_buf.display();
+            html.push_str(format!("<a href='{}'>{}</a><br/>", path_display, path_buf.file_name().unwrap().to_owned().into_string().unwrap()).as_str());
+        }
+    } else {
+        html = match fs::read_to_string(&from) {
+            Ok(x) => x,
+            Err(x) => from,
+        };
+
+        return HttpResponse::Ok()
+        .content_type("text/plain; charset=utf-8")
+        .body(html);
+    }
+
 
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
@@ -86,7 +99,7 @@ fn index() -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    // std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
     // std::fs::create_dir_all("./tmp").unwrap();
 
     let ip = "0.0.0.0:8000";
